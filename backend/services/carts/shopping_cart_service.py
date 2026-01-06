d288752b211f00b384d855728ccac8c798dbb99c
pranavkrishna05 <pranav2002krishna@gmail.com>
2026-01-06 11:26:32 +0530

    commited by n8n

---
+++ backend/services/carts/shopping_cart_service.py
@@ -0,0 +1,28 @@
+from typing import Optional, List
+import logging
+
+from backend.models.shopping_cart import ShoppingCart
+from backend.repositories.carts.shopping_cart_repository import ShoppingCartRepository
+
+class ShoppingCartService:
+    def __init__(self, shopping_cart_repository: ShoppingCartRepository):
+        self.shopping_cart_repository = shopping_cart_repository
+        self.logger = logging.getLogger(__name__)
+
+    def get_or_create_cart(self, user_id: int) -> ShoppingCart:
+        cart = self.shopping_cart_repository.get_cart_by_user_id(user_id)
+        if cart is None:
+            cart_id = self.shopping_cart_repository.create_cart(user_id, [])
+            cart = self.shopping_cart_repository.get_cart_by_cart_id(cart_id)
+        return ShoppingCart(**cart)
+
+    def add_product_to_cart(self, user_id: int, product_id: int) -> None:
+        cart = self.get_or_create_cart(user_id)
+        self.shopping_cart_repository.add_product_to_cart(cart.id, product_id)
+        self.logger.info("Added product %s to cart %s", product_id, cart.id)
+
+    def remove_product_from_cart(self, user_id: int, product_id: int) -> None:
+        cart = self.shopping_cart_repository.get_cart_by_user_id(user_id)
+        if cart:
+            self.shopping_cart_repository.remove_product_from_cart(cart['id'], product_id)
+            self.logger.info("Removed product %s from cart %s", product_id, cart['id'])
